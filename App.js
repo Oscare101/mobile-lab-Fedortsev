3e10f31d6cd1506d81cbbf9f57f12888e84c48fe
Oscare101 <kirillfedortsev2000@gmail.com>
2021-02-08 11:09:24 +0200

    1.1 lab

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import Home from './screens/Home'
+import Settings from './screens/Settings'
+import { NavigationContainer } from '@react-navigation/native'
+const Tab = createBottomTabNavigator()
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Settings" component={Settings} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -18,4 +24,4 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-});
+})
---
+++ README.md
@@ -0,0 +1,39 @@
+# lab 1.1 mobile app by Fedortsev
+
+## create app by comand
+
+`npx create-react-native-app mobile-lab`
+
+I am using react-native technology, was downloaded from official website
+
+## creating icon
+
+in react native we can not create icon, so i made it in figma
+
+[my icon](https://www.figma.com/file/knavGLyGUNVQ3uwNwNxl1D/Untitled?node-id=0%3A1) - open it in Figma
+
+## Navigation
+
+was created by library react navigation
+
+`npm install @react-navigation/native`
+
+for example will use bottom tab navigation
+
+`npm install @react-navigation/bottom-tabs`
+
+## start app
+
+`expo start`
+
+I am using expo for start app in local server on my PC and phone at the same time, it makes develop easier
+
+## add gitignore
+
+was created by comand `git init`
+
+## app output
+
+we can see bottom navigation and two screens: Home and Settings
+
+![alt text](img/1.1.jpg 'Описание будет тут')
