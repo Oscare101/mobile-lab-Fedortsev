c8966d07747b8e74341121e373273e76b50677d5
Oscare101 <kirillfedortsev2000@gmail.com>
2021-02-16 15:42:45 +0200

    lab5 finished

--- README.md
+++ README.md
@@ -2,6 +2,31 @@
 
 check my variant `(8127 % 6 + 1)` = 4
 
-![example](img/.jpg)
+# creating grid
+
+I was using `react-native-easy-grid` for this laboratory. Just playd with Col and Row and build view which I have on my variant
+
+# Add photos
+
+use `expo-image-picker` for get images from my phone's galary, which called after button 'add photo' press, use `useState` to get path for this photo and simple `Array` to store them
+
+Each `Image` element has own image path `Array[i]`
+
+After a choose photo, it fill in its place in grid
+
+#examples
+
+empty screen
+![example](img/5.1.jpg)
+
+window of choosing photo from my galary
+
+![example](img/5.2.jpg)
+
+photo appears
+
+![example](img/5.3.jpg)
+
+I had some problems with adding a video to Readme
 
 ## create gitignore, commit and push to git
--- screens/Lab5.js
+++ screens/Lab5.js
@@ -8,41 +8,82 @@ import {
   StyleSheet,
   Dimensions,
   ScrollView,
+  TouchableOpacity,
+  Platform,
 } from 'react-native'
 import { Col, Row, Grid } from 'react-native-easy-grid'
+import * as ImagePicker from 'expo-image-picker'
+import Constants from 'expo-constants'
 
 const width = Dimensions.get('window').width
-
+const IMG = []
 const Variant = () => {
   let vari = (8127 % 6) + 1
   return <Text style={styles.header}>Lab 5 variant - {vari}</Text>
 }
 
-export default function ImagePickerExample() {
+export default function Lab5() {
+  const [image, setImage] = useState(null)
+
+  useEffect(() => {
+    ;(async () => {
+      if (Platform.OS !== 'web') {
+        const {
+          status,
+        } = await ImagePicker.requestMediaLibraryPermissionsAsync()
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!')
+        }
+      }
+    })()
+  }, [])
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1,
+    })
+
+    console.log(result)
+    IMG.push(result.uri)
+    if (!result.cancelled) {
+      setImage(result.uri)
+    }
+  }
+  console.log(' = -----------')
+  console.log(IMG)
+  console.log('----------- = ')
   return (
     <>
       <Variant />
+      <TouchableOpacity onPress={pickImage}>
+        <View style={styles.add}>
+          <Text>add photo</Text>
+        </View>
+      </TouchableOpacity>
       <ScrollView style={{ flex: 1 }}>
         <Grid>
           <Col style={styles.col}>
             <Row style={styles.row}>
               <View style={styles.view}>
-                <Text>1.1</Text>
+                <Image source={{ uri: IMG[0] }} style={styles.img} />
               </View>
             </Row>
             <Row style={styles.row}>
               <View style={styles.view}>
-                <Text>1.2</Text>
+                <Image source={{ uri: IMG[3] }} style={styles.img} />
               </View>
             </Row>
             <Row style={styles.row}>
               <View style={styles.view}>
-                <Text>1.3</Text>
+                <Image source={{ uri: IMG[5] }} style={styles.img} />
               </View>
             </Row>
             <Row style={styles.row}>
               <View style={styles.view}>
-                <Text>1.4</Text>
+                <Image source={{ uri: IMG[6] }} style={styles.img} />
               </View>
             </Row>
           </Col>
@@ -50,29 +91,84 @@ export default function ImagePickerExample() {
             <Row style={styles.row}>
               <Col style={styles.col}>
                 <View style={styles.view}>
-                  <Text>2.1</Text>
+                  <Image source={{ uri: IMG[1] }} style={styles.img} />
                 </View>
               </Col>
               <Col style={styles.col}>
                 <View style={styles.view}>
-                  <Text>2.2</Text>
+                  <Image source={{ uri: IMG[2] }} style={styles.img} />
                 </View>
               </Col>
             </Row>
             <Row style={{ height: width / 1.5 }}>
               <View style={styles.view}>
-                <Text>2.3</Text>
+                <Image source={{ uri: IMG[4] }} style={styles.img} />
               </View>
             </Row>
             <Row style={styles.row}>
               <Col style={styles.col}>
                 <View style={styles.view}>
-                  <Text>2.4</Text>
+                  <Image source={{ uri: IMG[7] }} style={styles.img} />
                 </View>
               </Col>
               <Col style={styles.col}>
                 <View style={styles.view}>
-                  <Text>2.5</Text>
+                  <Image source={{ uri: IMG[8] }} style={styles.img} />
+                </View>
+              </Col>
+            </Row>
+          </Col>
+        </Grid>
+        <Grid>
+          <Col style={styles.col}>
+            <Row style={styles.row}>
+              <View style={styles.view}>
+                <Image source={{ uri: IMG[9] }} style={styles.img} />
+              </View>
+            </Row>
+            <Row style={styles.row}>
+              <View style={styles.view}>
+                <Image source={{ uri: IMG[12] }} style={styles.img} />
+              </View>
+            </Row>
+            <Row style={styles.row}>
+              <View style={styles.view}>
+                <Image source={{ uri: IMG[14] }} style={styles.img} />
+              </View>
+            </Row>
+            <Row style={styles.row}>
+              <View style={styles.view}>
+                <Image source={{ uri: IMG[15] }} style={styles.img} />
+              </View>
+            </Row>
+          </Col>
+          <Col style={{ width: width / 1.5 }}>
+            <Row style={styles.row}>
+              <Col style={styles.col}>
+                <View style={styles.view}>
+                  <Image source={{ uri: IMG[10] }} style={styles.img} />
+                </View>
+              </Col>
+              <Col style={styles.col}>
+                <View style={styles.view}>
+                  <Image source={{ uri: IMG[11] }} style={styles.img} />
+                </View>
+              </Col>
+            </Row>
+            <Row style={{ height: width / 1.5 }}>
+              <View style={styles.view}>
+                <Image source={{ uri: IMG[13] }} style={styles.img} />
+              </View>
+            </Row>
+            <Row style={styles.row}>
+              <Col style={styles.col}>
+                <View style={styles.view}>
+                  <Image source={{ uri: IMG[16] }} style={styles.img} />
+                </View>
+              </Col>
+              <Col style={styles.col}>
+                <View style={styles.view}>
+                  <Image source={{ uri: IMG[17] }} style={styles.img} />
                 </View>
               </Col>
             </Row>
@@ -85,10 +181,9 @@ export default function ImagePickerExample() {
 
 const styles = StyleSheet.create({
   view: {
-    borderWidth: 1,
-    borderColor: 'black',
     flex: 1,
     margin: 1,
+    overflow: 'hidden',
   },
   col: {
     width: width / 3,
@@ -100,5 +195,13 @@ const styles = StyleSheet.create({
     marginTop: 15,
     fontSize: 22,
     fontWeight: 'bold',
+  },
+  img: { width: '100%', height: '100%' },
+  add: {
+    backgroundColor: 'lightblue',
+    borderRadius: 5,
+    margin: 10,
+    alignItems: 'center',
+    padding: 5,
   },
 })
